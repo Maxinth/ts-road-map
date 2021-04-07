05d400179227082709ecc84af19ece590be36010
maxinth <tunde.j.adeleye@gmail.com>
2021-04-07 15:20:11 +0100

    using the lib property to compile down to an acceptable es version

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,8 @@
-function printMessage(msg) {
-    console.log(`Message: ${msg}`);
-}
-printMessage("Hello, TypeScript");
+var printMessage = function (msg) { return console.log("Message: " + msg); };
+var message = "Hello, TypeScript";
+printMessage(message);
+/* 🔥 - checking if a new feature gets transpiled down into older js code */
+var data = new Map();
+data.set("Bob", "London");
+data.set("Alice", "Paris");
+data.forEach(function (val, key) { return console.log(key + " lives in " + val); });
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
-function printMessage(msg: string): void {
-  console.log(`Message: ${msg}`);
-}
+let printMessage = (msg: string): void => console.log(`Message: ${msg}`);
+let message = "Hello, TypeScript";
+printMessage(message);
 
-printMessage("Hello, TypeScript");
+/* 🔥 - checking if a new feature gets transpiled down into older js code */
+let data = new Map();
+data.set("Bob", "London");
+data.set("Alice", "Paris");
+data.forEach((val, key) => console.log(`${key} lives in ${val}`));
