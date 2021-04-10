6571a299b795a302e37df1baab62eb589b352c1f
maxinth <tunde.j.adeleye@gmail.com>
2021-04-10 13:35:50 +0100

    asserting an unexpected type

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,17 @@
+// Asserting to an unexpected type
 function calculateTax(amount, format) {
     const calcAmount = amount * 1.2;
     return format ? `$${calcAmount.toFixed(2)} ` : calcAmount;
 }
 let taxNumber = calculateTax(500, false);
 let taxString = calculateTax(100, true);
+/*
+the boolean type specified in taxBoolean doesn't exist in the types calculateTax returns
+(string | number), however it can be asserted on the the value of calculateTax by first
+asserting to it as any and then as boolean
+
+*/
+let taxBoolean = calculateTax(100, false);
 console.log(`Number Value: ${taxNumber.toFixed(2)}`);
 console.log(`String Value: ${taxString.charAt(0)}`);
+console.log("taxBoolean = ", taxBoolean);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+// Asserting to an unexpected type
+
 function calculateTax(amount: number, format: boolean): string | number {
   const calcAmount = amount * 1.2;
   return format ? `$${calcAmount.toFixed(2)} ` : calcAmount;
@@ -6,5 +8,14 @@ function calculateTax(amount: number, format: boolean): string | number {
 let taxNumber: number = calculateTax(500, false) as number;
 let taxString: string = calculateTax(100, true) as string;
 
+/*
+the boolean type specified in taxBoolean doesn't exist in the types calculateTax returns
+(string | number), however it can be asserted on the the value of calculateTax by first 
+asserting to it as any and then as boolean
+
+*/
+let taxBoolean: boolean = (calculateTax(100, false) as any) as boolean;
+
 console.log(`Number Value: ${taxNumber.toFixed(2)}`);
 console.log(`String Value: ${taxString.charAt(0)}`);
+console.log("taxBoolean = ", taxBoolean);
