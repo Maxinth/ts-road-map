95f03c75a63fd7422b14bbf852a809bbe8a38598
maxinth <tunde.j.adeleye@gmail.com>
2021-04-11 14:00:13 +0100

    using optional parameter

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 /* Using Functions */
 
+// optional parameters
+
 function calculateTax(amount: number, discount?: number): number {
   return amount * 1.2 - (discount || 0);
 }
 
 let taxValue = calculateTax(100, 0);
 console.log(`2 args: ${taxValue}`);
+
+// calling said function leaving the optional parameter out
 taxValue = calculateTax(100);
 console.log(`1 arg: ${taxValue}`);
