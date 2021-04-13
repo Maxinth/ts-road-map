6a363152e8345a7b2ed766afe02ea866436ae548
maxinth <tunde.j.adeleye@gmail.com>
2021-04-13 12:08:56 +0100

    enabling strictNullChecks

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -1,4 +1,4 @@
 declare function calculateTax(amount: number): number;
 declare function writePrice(product: string, price: number): void;
-declare let prices: any[];
+declare let prices: never[];
 declare let names: string[];
