82c52093042c2917ab8429304f80648058df1355
maxinth <tunde.j.adeleye@gmail.com>
2021-04-17 20:14:06 +0100

    defining constant enums

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,2 @@
-// working with tuples
-function calculateTax(amount) {
-    return amount * 1.2;
-}
-function writePrice(product, price) {
-    console.log(`price for ${product} : $${price.toFixed(2)}`);
-}
-// using enum and number values
-var OtherEnum;
-(function (OtherEnum) {
-    OtherEnum[OtherEnum["First"] = 10] = "First";
-    OtherEnum[OtherEnum["Two"] = 20] = "Two";
-})(OtherEnum || (OtherEnum = {}));
-var Product;
-(function (Product) {
-    Product[Product["Hat"] = 11] = "Hat";
-    Product[Product["Gloves"] = 20] = "Gloves";
-    Product[Product["Umbrella"] = 31] = "Umbrella";
-})(Product || (Product = {}));
-let productValue = Product.Hat;
-if (typeof productValue === "number") {
-    console.log("Value is a number");
-}
-let unionValue = Product.Hat;
-if (typeof unionValue === "number") {
-    console.log("Value is a number");
-}
+let productVal = 0 /* Hat */;
+console.log(`productVal = ${Product.}`);
--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,9 @@
-// working with tuples
-
-function calculateTax(amount: number): number {
-  return amount * 1.2;
+const enum Product {
+  Hat,
+  Gloves,
+  Umbrella,
 }
 
-function writePrice(product: string, price: number): void {
-  console.log(`price for ${product} : $${price.toFixed(2)}`);
-}
+let productVal = Product.Hat;
 
-// using enum and number values
-
-enum OtherEnum {
-  First = 10,
-  Two = 20,
-}
-
-enum Product {
-  Hat = OtherEnum.First + 1,
-  Gloves = 20,
-  Umbrella = Hat + Gloves,
-}
-
-let productValue: Product = Product.Hat;
-if (typeof productValue === "number") {
-  console.log("Value is a number");
-}
-
-let unionValue: number | Product = Product.Hat;
-if (typeof unionValue === "number") {
-  console.log("Value is a number");
-}
+console.log(`productVal = ${Product.}`);
