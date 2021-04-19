1f6baedc99d46ebef842f31a4ec9e1b6fba958d8
maxinth <tunde.j.adeleye@gmail.com>
2021-04-19 14:20:22 +0100

    rolled back to initial view

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,1 @@
 //structural types
-
-class Person {
-  name: string;
-  age: number;
-}
-class Customer {
-  name: string;
-}
-
-const customer: Customer = new Person();
-console.log("more test");
