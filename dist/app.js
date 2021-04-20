bf8bec8defad255b9e7a4e1b7dd17e31f8675cf3
maxinth <tunde.j.adeleye@gmail.com>
2021-04-20 15:12:14 +0100

    more fxn

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,5 @@
 // Full stack react+Ts+Node
-// functions with no return value
+// functions with no return value - returns undefined
 function eat(calories) {
     console.log(`I ate ${calories} eggs`);
 }
@@ -14,3 +14,6 @@ function drink(cup) {
 eat(4);
 eatMore(5);
 console.log(`I drank ${drink(3)} cups of water`);
+console.log(`typeof = eat function ${typeof eat(3)}`);
+console.log(`typeof  eatMore with 5 as arg = ${typeof eatMore(5)} `);
+console.log(`typeof drink fxn = ${typeof drink(3)} `);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 // Full stack react+Ts+Node
-// functions with no return value
+// functions with no return value - returns undefined
 function eat(calories: number): void {
   console.log(`I ate ${calories} eggs`);
 }
@@ -16,3 +16,7 @@ function drink(cup: number): number {
 eat(4);
 eatMore(5);
 console.log(`I drank ${drink(3)} cups of water`);
+
+console.log(`typeof = eat function ${typeof eat(3)}`);
+console.log(`typeof  eatMore with 5 as arg = ${typeof eatMore(5)} `);
+console.log(`typeof drink fxn = ${typeof drink(3)} `);
