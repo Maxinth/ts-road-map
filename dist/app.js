e5200017b6244443747ed2a0ed1668768a9f7511
maxinth <tunde.j.adeleye@gmail.com>
2021-05-22 14:11:35 +0100

    using an empty array

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,11 @@
-// essential ts
 function calculateTax(amount) {
     return amount * 1.2;
 }
 function writePrice(product, price) {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
-// let prices: number[] = [100, 75, 42];
-// let names: string[] = ["Hat", "Gloves", "umbrellas"];
-// prices.forEach((price: number, index: number) => {
-//   writePrice(names[index], calculateTax(price));
-// });
-let prices = [100, 75, 42, "20"];
+let prices = [];
+prices.push(...[100, 75, 42, "20"]);
 let names = ["Hat", "Gloves", "Umbrella", "Sunglasses"];
 prices.forEach((price, index) => {
     writePrice(names[index], calculateTax(price));
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,15 @@
-// essential ts
 function calculateTax(amount: number): number {
   return amount * 1.2;
 }
 
-function writePrice(product: string, price: string | number): void {
+function writePrice(product: string, price: number): void {
   console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
 
-// let prices: number[] = [100, 75, 42];
-// let names: string[] = ["Hat", "Gloves", "umbrellas"];
+let prices = [];
+prices.push(...[100, 75, 42, "20"]);
 
-// prices.forEach((price: number, index: number) => {
-//   writePrice(names[index], calculateTax(price));
-// });
-
-let prices: (string | number)[] = [100, 75, 42, "20"];
 let names = ["Hat", "Gloves", "Umbrella", "Sunglasses"];
-prices.forEach((price: string | number, index) => {
+prices.forEach((price, index) => {
   writePrice(names[index], calculateTax(price));
 });
