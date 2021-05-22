09389e81ab30e67740e275c5743aa4629c26e2b7
maxinth <tunde.j.adeleye@gmail.com>
2021-05-22 19:27:21 +0100

    using typeguard with tuples

--- dist/app.js
+++ dist/app.js
@@ -4,3 +4,22 @@ function calculateTax(amount) {
 function writePrice(product, price) {
     console.log(`Price for ${product}: $${price.toFixed(2)}`);
 }
+let hat = ["Hat", 100];
+let gloves = ["Gloves", 75];
+let products = [hat, gloves];
+let tupleUnion = [true, false, hat, ...products];
+tupleUnion.forEach((elem) => {
+    if (elem instanceof Array) {
+        elem.forEach((tupleElem) => {
+            if (typeof tupleElem === "string") {
+                console.log(`String Value: ${tupleElem}`);
+            }
+            else {
+                console.log(`Number Value: ${tupleElem}`);
+            }
+        });
+    }
+    else if (typeof elem === "boolean") {
+        console.log(`Boolean Value: ${elem}`);
+    }
+});
