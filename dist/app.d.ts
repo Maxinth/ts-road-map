712bd62f55e3322fe1b5d8911f13c6e4d7cd3689
maxinth <tunde.j.adeleye@gmail.com>
2021-05-24 11:25:54 +0100

    mixing values in a litera

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -1,2 +1,10 @@
+declare type Lit = 1 | 2 | 3 | 4;
 declare function calculatePrice(quantity: 1 | 2, price: number): number;
 declare let total: number;
+declare function getRandomVal(): Lit;
+declare enum City {
+    London = "LON",
+    Paris = " PAR",
+    Chicago = "CHI"
+}
+declare function getMixedValue(): 1 | "Hello" | true | City.London;
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+type Lit = 1 | 2 | 3 | 4;
+
 function calculatePrice(quantity: 1 | 2, price: number): number {
   return quantity * price;
 }
@@ -5,3 +7,28 @@ function calculatePrice(quantity: 1 | 2, price: number): number {
 let total = calculatePrice(2, 19.99);
 
 console.log(`Price: ${total}`);
+
+function getRandomVal(): Lit {
+  return (Math.floor(Math.random() * 4) + 1) as Lit;
+}
+
+enum City {
+  London = "LON",
+  Paris = " PAR",
+  Chicago = "CHI",
+}
+
+function getMixedValue(): 1 | "Hello" | true | City.London {
+  switch (getRandomVal()) {
+    case 1:
+      return 1;
+    case 2:
+      return "Hello";
+    case 3:
+      return true;
+    case 4:
+      return City.London;
+  }
+}
+
+console.log(`Value: ${getMixedValue()}`);
