8d650ddf56e56d2e5743a996c98d74f59caecc47
maxinth <tunde.j.adeleye@gmail.com>
2021-05-25 10:31:12 +0100

    type guarding by checking properties

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,18 @@
+// union type for common types between Person and Product
+// type UnionType = {
+//   id: number | string;
+//   name: string;
+// };
 let hat = { id: 1, name: "Hat", price: 100 };
 let gloves = { id: 2, name: "Gloves", price: 75 };
 let umbrella = { id: 3, name: "Umbrella", price: 30 };
 let bob = { id: "bsmith", name: "Bob", city: "London" };
 let dataItems = [hat, gloves, umbrella, bob];
 dataItems.forEach((item) => {
-    console.log(`ID: ${item.id}, Name: ${item.name} \n `);
+    if ("city" in item) {
+        console.log(`Person : ${item.name}: ${item.city}`);
+    }
+    else {
+        console.log(`Product : ${item.name}: ${item.price}`);
+    }
 });
