93e587557c6e9fb0a5c45bd0923818d9f717308b
maxinth <tunde.j.adeleye@gmail.com>
2021-05-30 07:43:50 +0100

    merging different types - first go

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -2,27 +2,13 @@ declare type Person = {
     id: string;
     name: string;
     city: string;
+    contact: number;
 };
 declare type Employee = {
     id: string;
     company: string;
     dept: string;
+    contact: string;
 };
 declare type EmployedPerson = Person & Employee;
-declare function correlateData(peopleData: Person[], staff: Employee[]): EmployedPerson[];
-declare let people: Person[];
-declare let employees: Employee[];
-declare let dataItems: EmployedPerson[];
-declare function writePerson(per: Person): void;
-declare function writeEmployees(emp: Employee): void;
-/**
- * writePerson expects an argument whose type is Person
- * writeEmployees also expects an argument whose type is Employee
- *
- * In the forEach callback, item whose type is EmployedPerson is passed in to both
- * functions
- *
-
-Because an intersection combines features from multiple types, an object that conforms to the intersection
-shape also conforms to each of the types in the intersection.
- */
+declare let typeTest: never;
--- src/app.ts
+++ src/app.ts
@@ -2,60 +2,14 @@ type Person = {
   id: string;
   name: string;
   city: string;
+  contact: number;
 };
-
 type Employee = {
   id: string;
   company: string;
   dept: string;
+  contact: string;
 };
 
 type EmployedPerson = Person & Employee;
-
-function correlateData(
-  peopleData: Person[],
-  staff: Employee[]
-): EmployedPerson[] {
-  const defaults = { company: "None", dept: "None" };
-  return peopleData.map((p) => ({
-    ...p,
-    ...(staff.find((e) => e.id === p.id) || { ...defaults, id: p.id }),
-  }));
-}
-
-let people: Person[] = [
-  { id: "bsmith", name: "Bob Smith", city: "London" },
-  { id: "ajones", name: "Alice Jones", city: "Paris" },
-  { id: "dpeters", name: "Dora Peters", city: "New York" },
-];
-let employees: Employee[] = [
-  { id: "bsmith", company: "Acme Co", dept: "Sales" },
-  { id: "dpeters", company: "Acme Co", dept: "Development" },
-];
-
-let dataItems: EmployedPerson[] = correlateData(people, employees);
-
-function writePerson(per: Person): void {
-  console.log(`Person: ${per.id}, ${per.name} ${per.city}`);
-}
-
-function writeEmployees(emp: Employee): void {
-  console.log(`Employees: ${emp.id} ${emp.company} ${emp.dept}`);
-}
-
-dataItems.forEach((item) => {
-  writePerson(item);
-  writeEmployees(item);
-});
-
-/**
- * writePerson expects an argument whose type is Person
- * writeEmployees also expects an argument whose type is Employee
- *
- * In the forEach callback, item whose type is EmployedPerson is passed in to both
- * functions
- * 
-
-Because an intersection combines features from multiple types, an object that conforms to the intersection 
-shape also conforms to each of the types in the intersection.
- */
+let typeTest = ({} as EmployedPerson).contact;
