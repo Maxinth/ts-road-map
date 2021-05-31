0686a4514a9552d19e0df485f0fa6f26c8a39ced
maxinth <tunde.j.adeleye@gmail.com>
2021-05-31 12:26:49 +0100

    using the normal js private field

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,29 @@
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to set private field on non-instance");
+    }
+    privateMap.set(receiver, value);
+    return value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to get private field on non-instance");
+    }
+    return privateMap.get(receiver);
+};
+var _dept;
 class Employee {
     constructor(id, name, dept, city) {
+        _dept.set(this, void 0); // private field syntax for js
         this.id = id;
         this.name = name;
-        this.dept = dept;
+        __classPrivateFieldSet(this, _dept, dept);
         this.city = city;
     }
     writeDept() {
-        console.log(`${this.name} works in ${this.dept} department`);
+        console.log(`${this.name} works in ${__classPrivateFieldGet(this, _dept)} department`);
     }
 }
+_dept = new WeakMap();
 let salesEmployee = new Employee("fvega", "Fidel Vega", "Sales", "Paris");
 salesEmployee.writeDept();
--- src/app.ts
+++ src/app.ts
@@ -7,18 +7,18 @@ type Person = {
 class Employee {
   public id: string;
   public name: string;
-  private dept: string;
+  #dept: string; // private field syntax for js
   public city: string;
 
   constructor(id: string, name: string, dept: string, city: string) {
     this.id = id;
     this.name = name;
-    this.dept = dept;
+    this.#dept = dept;
     this.city = city;
   }
 
   writeDept() {
-    console.log(`${this.name} works in ${this.dept} department`);
+    console.log(`${this.name} works in ${this.#dept} department`);
   }
 }
 
