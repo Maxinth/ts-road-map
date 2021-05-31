401e248498c29db373a0b7b14b788234288f3201
maxinth <tunde.j.adeleye@gmail.com>
2021-05-31 12:57:00 +0100

    using objects with common superclass

--- dist/app.js
+++ dist/app.js
@@ -17,13 +17,38 @@ class Employee extends Person {
         console.log(`${this.name} works in ${this.dept} department`);
     }
 }
+class Customer extends Person {
+    constructor(id, name, city, creditLimit) {
+        super(id, name, city);
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.creditLimit = creditLimit;
+    }
+}
+class Supplier extends Person {
+    constructor(id, name, city, companyName) {
+        super(id, name, city);
+        this.id = id;
+        this.name = name;
+        this.city = city;
+        this.companyName = companyName;
+    }
+}
 let data = [
-    new Person("smith", "Bob Smith", "London"),
-    new Employee("auto", "Smart Autos", "sales", "paris"),
+    new Employee("fvega", "Fidel Vega", "Sales", "Paris"),
+    new Customer("ajones", "Alice Jones", "London", 500),
 ];
-data.forEach(item => {
+data.push(new Supplier("dpeters", "Dora Peters", "New York", "Acme"));
+data.forEach((item) => {
     console.log(`Person: ${item.name}, ${item.city}`);
     if (item instanceof Employee) {
         item.writeDept();
+    }
+    else if (item instanceof Customer) {
+        console.log(`Customer ${item.name} has ${item.creditLimit} limit`);
+    }
+    else if (item instanceof Supplier) {
+        console.log(`Supplier ${item.name} works for ${item.companyName}`);
     }
 });
--- src/app.ts
+++ src/app.ts
@@ -15,15 +15,39 @@ class Employee extends Person {
     console.log(`${this.name} works in ${this.dept} department`);
   }
 }
-
-let data = [
-  new Person("smith", "Bob Smith", "London"),
-  new Employee("auto", "Smart Autos", "sales", "paris"),
-];
-
-data.forEach(item => {
-  console.log(`Person: ${item.name}, ${item.city}`)
-  if(item instanceof Employee){
-    item.writeDept()
+class Customer extends Person {
+  constructor(
+    public readonly id: string,
+    public name: string,
+    public city: string,
+    public creditLimit: number
+  ) {
+    super(id, name, city);
   }
-})
+}
+class Supplier extends Person {
+  constructor(
+    public readonly id: string,
+    public name: string,
+    public city: string,
+    public companyName: string
+  ) {
+    super(id, name, city);
+  }
+}
+let data = [
+  new Employee("fvega", "Fidel Vega", "Sales", "Paris"),
+  new Customer("ajones", "Alice Jones", "London", 500),
+];
+data.push(new Supplier("dpeters", "Dora Peters", "New York", "Acme"));
+
+data.forEach((item) => {
+  console.log(`Person: ${item.name}, ${item.city}`);
+  if (item instanceof Employee) {
+    item.writeDept();
+  } else if (item instanceof Customer) {
+    console.log(`Customer ${item.name} has ${item.creditLimit} limit`);
+  } else if (item instanceof Supplier) {
+    console.log(`Supplier ${item.name} works for ${item.companyName}`);
+  }
+});
