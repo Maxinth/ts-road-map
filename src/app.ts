9ad33d1666a10f263fd128f52475f18af1cb82ba
maxinth <tunde.j.adeleye@gmail.com>
2021-06-01 23:03:10 +0100

    interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,35 @@
-abstract class Person {
-  constructor(public id: string, public name: string, public city: string) {}
-  getDetails(): string {
-    return `${this.name} ${this.getSpecificDetails()}`;
-  }
-
-  // abstract method MUST exist between abstract classes
-  // and must be implemented by all classes extending the abstract class
-  // i.e Employee, Customer, Supplier
-  abstract getSpecificDetails(): string;
+interface Person {
+  name: string;
+  getDetails(): string;
 }
-class Employee extends Person {
+class Employee implements Person {
   constructor(
     public readonly id: string,
     public name: string,
     private dept: string,
     public city: string
-  ) {
-    super(id, name, city);
-  }
-  getSpecificDetails() {
-    return `works in ${this.dept}`;
+  ) {}
+  getDetails() {
+    return `${this.name} works in ${this.dept}`;
   }
 }
-class Customer {
+class Customer implements Person {
   constructor(
     public readonly id: string,
     public name: string,
     public city: string,
     public creditLimit: number
   ) {}
-}
-class Supplier extends Person {
-  constructor(
-    public readonly id: string,
-    public name: string,
-    public city: string,
-    public companyName: string
-  ) {
-    super(id, name, city);
-  }
-  getSpecificDetails() {
-    return `works for ${this.companyName}`;
+  getDetails() {
+    return `${this.name} has ${this.creditLimit} limit`;
   }
 }
-let data: (Person | Customer)[] = [
+
+let data: Person[] = [
   new Employee("fvega", "Fidel Vega", "Sales", "Paris"),
   new Customer("ajones", "Alice Jones", "London", 500),
 ];
-data.push(new Supplier("dpeters", "Dora Peters", "New York", "Acme"));
 
 data.forEach((item) => {
-  if (item instanceof Person) {
-    console.log(item.getDetails());
-  } else {
-    console.log(`Customer: ${item.name}`);
-  }
+  console.log(item.getDetails());
 });
