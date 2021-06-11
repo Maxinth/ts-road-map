41e746f9d45ed85839429fc0bf4631bd1f5ce468
maxinth <tunde.j.adeleye@gmail.com>
2021-06-11 12:34:36 +0100

    mastering ts

--- dist/app.js
+++ dist/app.js
@@ -1,32 +1,6 @@
-class Employee {
-    constructor(name, company) {
-        this.name = name;
-        this.company = company;
-        // no statements required
-    }
-    getDetails() {
-        return `${this.name} works for ${this.company}`;
-    }
-}
-class SportsProduct {
-    constructor(name, category, price) {
-        this.name = name;
-        this.category = category;
-        this.price = price;
-        // no statements required
-    }
-}
-class ProductGroup {
-    constructor(...initialProducts) {
-        initialProducts.forEach((p) => (this[p[0]] = p[1]));
-    }
-}
-let group = new ProductGroup([
-    "shoes",
-    new SportsProduct("Shoes", "Running", 90.5),
-]);
-group.hat = new SportsProduct("Hat", "Skiing", 20);
-if (group.hat && group.boots) {
-    let total = group.hat.price + group.boots.price;
-    console.log(`Total: ${total}`);
-}
+/**
+ * Mastering typeScript
+ */
+console.log("hello typescript");
+var version = "es6";
+console.log("hello " + version + " Typescript");
--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,8 @@
-interface Person {
-  name: string;
-  getDetails(): string;
-}
-interface Product {
-  name: string;
-  price: number;
-}
-class Employee implements Person {
-  constructor(public name: string, public company: string) {
-    // no statements required
-  }
-  getDetails() {
-    return `${this.name} works for ${this.company}`;
-  }
-}
-class SportsProduct implements Product {
-  constructor(
-    public name: string,
-    public category: string,
-    public price: number
-  ) {
-    // no statements required
-  }
-}
+/**
+ * Mastering typeScript
+ */
 
-class ProductGroup {
-  constructor(...initialProducts: [string, Product][]) {
-    initialProducts.forEach((p) => (this[p[0]] = p[1]));
-  }
-  [propertyName: string]: Product;
-}
-let group = new ProductGroup([
-  "shoes",
-  new SportsProduct("Shoes", "Running", 90.5),
-]);
-group.hat = new SportsProduct("Hat", "Skiing", 20);
-if (group.hat && group.boots) {
-  let total = group.hat.price + group.boots.price;
-  console.log(`Total: ${total}`);
-}
+console.log("hello typescript");
+
+var version = `es6`;
+console.log(`hello ${version} Typescript`);
