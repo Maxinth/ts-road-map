7b540842b68de6eb52fd638cd19444d7c7007338
maxinth <tunde.j.adeleye@gmail.com>
2021-06-17 21:44:19 +0100

    unknown type - second instance

--- dist/app.js
+++ dist/app.js
@@ -7,4 +7,4 @@ console.log("type of u = " + typeof u);
 u = 1;
 console.log("type of u = " + typeof u);
 var aNumber2;
-// aNumber2 = u ;
+// aNumber2 = u;
--- src/app.ts
+++ src/app.ts
@@ -7,4 +7,4 @@ console.log(`type of u = ${typeof u}`);
 u = 1;
 console.log(`type of u = ${typeof u}`);
 let aNumber2: number;
-// aNumber2 = u ;
+// aNumber2 = u;
