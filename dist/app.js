6d106e0e78b74ac53c0abbe23897d2c1ee8ea6fa
maxinth <tunde.j.adeleye@gmail.com>
2021-06-22 08:17:29 +0100

    reverted to initial setup

--- dist/app.js
+++ dist/app.js
@@ -5,12 +5,12 @@
  * INTERFACES , CLASSES, INHERITANCE AND MODULES
  */
 /*
-for IdNameClass to implement IDerivedFromBase it MUST HAVE THE 2 FIELDS IDerived
-from base has
+
+git add . && git commit -m " " && clear
+git add . && git commit -m " " && clear
+git add . && git commit -m " " && clear
+
+
+
+
 */
-class IdNameClass {
-    constructor() {
-        this.name = "nameString";
-        this.id = 0;
-    }
-}
