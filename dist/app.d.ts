d34b160c467c4bb494983386e48147d1dcc6d5df
maxinth <tunde.j.adeleye@gmail.com>
2021-06-25 22:06:17 +0100

    recreated the Concatenator class

--- dist/app.d.ts
+++ dist/app.d.ts
@@ -3,3 +3,8 @@
  * Mastering typeScript
  * GENERICS
  */
+declare class Concatenator<T extends Array<string> | Array<number>> {
+    concatenateArray(items: T): string;
+}
+declare let concator: Concatenator<string[] | number[]>;
+declare let concatResult: string;
