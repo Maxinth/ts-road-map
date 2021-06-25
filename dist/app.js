8f7e1668b7c05de90b308f9eb348fe165c5cc2a8
maxinth <tunde.j.adeleye@gmail.com>
2021-06-25 22:05:33 +0100

    reverted to initial setup

--- dist/app.js
+++ dist/app.js
@@ -18,23 +18,8 @@ git add . && git commit -m " " && clear
 git add . && git commit -m " " && clear
 git add . && git commit -m " " && clear
 git add . && git commit -m " " && clear
-git add . && git commit -m " " && clear
+git add . && git commit -m "reverted to initial setup" && clear
 
 
 
 */
-class Concatenator {
-    concatenateArray(items) {
-        let returnString = "";
-        for (let i = 0; i < items.length; i++) {
-            returnString += i > 0 ? "," : "";
-            returnString += items[i].toString();
-        }
-        return returnString;
-    }
-}
-let concator = new Concatenator();
-let concatResult = concator.concatenateArray(["first", "second", "third"]);
-console.log(`concatResult = ${concatResult}`);
-concatResult = concator.concatenateArray([1000, 200, 3000]);
-console.log(`concatResult = ${concatResult}`);
