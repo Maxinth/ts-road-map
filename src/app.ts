2b185d40a37e8b8a0c5111eda4d8a24c6405a4c4
maxinth <tunde.j.adeleye@gmail.com>
2021-06-28 15:29:49 +0100

    rolled back to initial setup

--- src/app.ts
+++ src/app.ts
@@ -21,21 +21,3 @@ git add . && git commit -m " " && clear
 
 
 */
-class Concatenator<T extends Array<string> | Array<number>> {
-  public concatenateArray(items: T): string {
-    let returnString = "";
-    for (let i = 0; i < items.length; i++) {
-      returnString += i > 0 ? "," : "";
-      returnString += items[i].toString();
-    }
-    return returnString;
-  }
-}
-
-let concator = new Concatenator();
-
-let concatResult = concator.concatenateArray(["first", "second", "third"]);
-console.log(`concatResult = ${concatResult}`);
-
-concatResult = concator.concatenateArray([1000, 200, 3000]);
-console.log(`concatResult = ${concatResult}`);
