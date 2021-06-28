c5cf13a074dd259df1040172e2ae2da20393ad39
maxinth <tunde.j.adeleye@gmail.com>
2021-06-28 15:28:52 +0100

    more m-tests

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ git add . && git commit -m " " && clear
 git add . && git commit -m " " && clear
 git add . && git commit -m " " && clear
 git add . && git commit -m " " && clear
-git add . && git commit -m " " && clear
+
 
 
 
@@ -21,6 +21,26 @@ git add . && git commit -m " " && clear
 
 
 */
+class Concatenator<T extends Array<string> | Array<number>> {
+  public concatenateArray(items: T): string {
+    let returnString = "";
+    for (let i = 0; i < items.length; i++) {
+      returnString += i > 0 ? "," : "";
+      returnString += items[i].toString();
+    }
+    return returnString;
+  }
+}
+
+let concator = new Concatenator();
+
+let concatResult = concator.concatenateArray(["first", "second", "third"]);
+console.log(`concatResult = ${concatResult}`);
+
+concatResult = concator.concatenateArray([1000, 200, 3000]);
+console.log(`concatResult = ${concatResult}`);
+
+console.log("third go");
 class Concatenator<T extends Array<string> | Array<number>> {
   public concatenateArray(items: T): string {
     let returnString = "";
